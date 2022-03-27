39c676e5595131f0bed31413210a225b7968bbf8
Elizark-m <85119923+Elizark-m@users.noreply.github.com>
2022-03-27 18:52:15 +0400

    Update Form1.cs

--- Hopfield_Neuronet/Form1.cs
+++ Hopfield_Neuronet/Form1.cs
@@ -10,7 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace NetworkofHopfild
+namespace Hopfield_Neuronet
 {
     public partial class Form1 : Form
     {
@@ -23,171 +23,175 @@ namespace NetworkofHopfild
         {
             InitializeComponent();
 
-            //инициализация формы
             Init();
 
-            //запускаем нейронную сеть?
             StartNetwork();
         }
-
+        
+        // prepare for neuronet work
         private void Init()
         {
-            //указываем директорию с изображниями
+            // save path to damage picture
             DirectoryInfo dir = new DirectoryInfo(@"..\..\damage");
-            //записываем в массив все файлы директории dir с расширением .bmp
+            // save files .bmp names in array
             FileInfo[] f = dir.GetFiles("*.bmp");
-            //в цикле по массиву файлов f добавляем в столбец рисунков названия файлов
+            // add saved names in form list
             for (int i = 0; i < f.Length; i++)
             {
                 listPictures.Items.Add(f[i].Name);
             }
         }
 
-
+        // 
         private void StartNetwork()
         {
-            //создаем новую базу с картинками
+            // create picture dataset
             dt = new DataClass();
-            //записываем исходные картинки в массив inputs
+            // save picture data from dataset
             inputs = dt.GetImg();
-            //создаем новую сеть с входными данными в виде двойного массива inputs
+            // create neuronet with learning dataset
             net = new Network(inputs);
         }
 
         private void listPictures_SelectedIndexChanged(object sender, EventArgs e)
         {
-            //запоминаем полное имя выбранного файла
+            // save selected picture name 
             string name = listPictures.SelectedItem.ToString();
-            //выбираем картинку с выбранным именем в директории damage
+            // choose picture file from damaged picture directory by name
             Image img = getImg(name, @"..\..\damage");
-            //размер = 15
+            // drawing picture size between points
             int size = 15;
             
-            //создаем битовую карту изображения по его размерам блока для поврежденной картинки
+            // create bitmap from selected image
             Bitmap bmp1 = new Bitmap(img, damagePic.Width, damagePic.Height);
 
-            //отрисовываем битовую карту изображения
+            // draw selected image
             g = Graphics.FromImage(bmp1);
-            //сохраняем битовую карту изображения в виде массива arrImg
+            // save selected image bitmap in array
             double[] arrImg = dt.GetImg((Bitmap)img);
 
-            //отрисовываем карту по массиву arrImg
+            // draw damage picture
             DrawRectGrid(g, 10, 10, size, arrImg);
-            //вставляем поученную картинку в блок damagePic 
+            // add result picture in form 
             damagePic.Image = bmp1;
     
         }
-
+        
+        // take image from path by name
         private Image getImg(string name, string direct)
         {
-            //находим директорию по пути
+            // take directory by path
             DirectoryInfo dir = new DirectoryInfo(direct);
-            //выбираем первый файл с указанным именем
+            // take file by name
             FileInfo f = dir.GetFiles(name).First();
-            //открываем картинку по полному имени файла
+            // save image from file
             Image img = Image.FromFile(f.FullName);
-            //возвращаем картинку
+            // return result
             return img;
         }
         
+        // drawing recovered picture on button click
         private void recoverButton_Click(object sender, EventArgs e)
         {
-            //запоминаем полное имя выбранного файла
+            // save selected picture name
             string name = listPictures.SelectedItem.ToString();
-            //выбираем картинку с выбранным именем в директории damage
+            // take damage picture from path by name
             Image img = getImg(name, @"..\..\damage");
-            //размер = 15
+            // drawing picture size between points
             int size = 15;
 
-            //создаем битовую карту изображения по его размерам блока для восстановленной картинки
+            // create bitmap size of recover picture
             Bitmap bmp2 = new Bitmap(img, recoverPic.Width, recoverPic.Height);
 
-            //сохраняем битовую карту изображения в виде массива damage
+            // save damage picture bitmap in array
             double[] damage = dt.GetImg((Bitmap)img);
-            //определяем нарушение? картинки
+            // define and recover damage picture array
             double[] res = net.Definition(damage);
 
-            //подготавливаем карту к отрисовке блока с исправленной картинкой
             g = Graphics.FromImage(bmp2);
 
             //double[] selectedImgArr = inputs[listPictures.SelectedIndex];
-            //отрисовываем исправленную картнику
+            // draw recover image
             DrawRectGrid(g, 10, 10, size, res);
-            //сохраняем исправленную картинку в блок исправленного изображения
+            // add recover image in form
             recoverPic.Image = bmp2;
         }
-         
+        
+        // filling rectange from array by rule
         public void DrawRectGrid(Graphics gr, float xmax, float ymax, int height, double[] weigths)
         {
-            //задаем начальные значения координат
+            // set start coordinates
             int x = 0, y = 0;
-            //отчищаем изображение
+            // clear image
             gr.Clear(Color.White);
             
-            //в цикле размеру картинки 
+            // foreach element in image
             for (int row = 0, i = 0; row < xmax; row++)
             {
                 for (int col = 0; col < ymax; col++)
                 {
-                    //выставляем необходимые значения
+                    //set coordinates current value
                     y = col * height;
                     x = row * height;
 
-                    //инициализируем кисть
+                    // initialize brush
                     Brush brush;
-                    //если значение веса = '1', то цвет кисти становиться черным, иначе белым
+                    // if current image element value = '1', brush color set black, else color set white
                     if (weigths[i] == 1) brush = new SolidBrush(Color.Black);
                     else brush = new SolidBrush(Color.White);
-                    //отрисовываем квадрат выбраной кистью по координатам, размером height * height
+                    // fill rectangle size of height * height at current coordinates by brush color
                     gr.FillRectangle(brush, x, y, height, height);
-
-                    //Pen pen = new Pen(Color.Black);
-
-                    //gr.DrawRectangle(pen, x, y, height, height);
 
                     i++;
                 }
             }
         }
 
-
+        // Scale image to specified size
         private Image ScaleImage(Image source, int width, int height)
         {
-            //создаем битовую карту размером width * height
+            // create future image bitmap
             Image dest = new Bitmap(width, height);
-            //
+            
             using (Graphics gr = Graphics.FromImage(dest))
             {
-                // Очищаем экран
+                // clear bitmap
                 gr.FillRectangle(Brushes.White, 0, 0, width, height);  
+                // choose high quality compression mode
                 gr.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
-
+                
+                // save sourse and result image size
                 float srcwidth = source.Width;
                 float srcheight = source.Height;
                 float dstwidth = width;
                 float dstheight = height;
-
-                if (srcwidth <= dstwidth && srcheight <= dstheight)  // Исходное изображение меньше целевого
+                
+                // draw image with compression
+                // if sourse image less target
+                if (srcwidth <= dstwidth && srcheight <= dstheight)  
                 {
                     int left = (width - source.Width) / 2;
                     int top = (height - source.Height) / 2;
                     gr.DrawImage(source, left, top, source.Width, source.Height);
                 }
-                else if (srcwidth / srcheight > dstwidth / dstheight)  // Пропорции исходного изображения более широкие
+                // if sourse image width more then target
+                else if (srcwidth / srcheight > dstwidth / dstheight)  
                 {
                     float cy = srcheight / srcwidth * dstwidth;
                     float top = ((float)dstheight - cy) / 2.0f;
                     if (top < 1.0f) top = 0;
                     gr.DrawImage(source, 0, top, dstwidth, cy);
                 }
-                else  // Пропорции исходного изображения более узкие
+                // if sourse image width less then target
+                else
                 {
                     float cx = srcwidth / srcheight * dstheight;
                     float left = ((float)dstwidth - cx) / 2.0f;
                     if (left < 1.0f) left = 0;
                     gr.DrawImage(source, left, 0, cx, dstheight);
                 }
-
+                
+                // return result
                 return dest;
             }
         }
